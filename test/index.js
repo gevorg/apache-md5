4c08d4210552fd5b02c273df54d3f88fbb5e9b73
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2016-06-18 04:40:39 +0400

    Proper tests.

--- test/index.js
+++ test/index.js
@@ -10,25 +10,25 @@ import md5 from '../src/index'
 describe('apache-md5', function () {
     // Test for valid password.
     it('valid password', function () {
-        let encrypted = md5("su/P3R%se#ret!", "$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
+        const encrypted = md5("su/P3R%se#ret!", "$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
         expect(encrypted).to.equal("$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
     });
 
     // Test for valid short password.
     it('valid short password', function () {
-        let encrypted = md5("123456", "$1$VV5.4y5.$JbhytGQBPmDHBbrSjF2i7.");
+        const encrypted = md5("123456", "$1$VV5.4y5.$JbhytGQBPmDHBbrSjF2i7.");
         expect(encrypted).to.equal("$1$VV5.4y5.$JbhytGQBPmDHBbrSjF2i7.");
     });
 
     // Test for invalid password.
     it('invalid password', function () {
-        let encrypted = md5("invalidPass", "$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
+        const encrypted = md5("invalidPass", "$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
         expect(encrypted).to.not.equal("$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
     });
 
     // Test for invalid short password.
     it('invalid short password', function () {
-        let encrypted = md5("passw0rdpa55wore", "$1$VV5.4y5.$9981ZZhKTHmeXFKQur4cV0");
+        const encrypted = md5("passw0rdpa55wore", "$1$VV5.4y5.$9981ZZhKTHmeXFKQur4cV0");
         expect(encrypted).to.not.equal("$1$VV5.4y5.$9981ZZhKTHmeXFKQur4cV0");
     });
 });
