ec8691f3d3544f78686ef5a60a56af9e60d41658
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2020-02-04 01:07:37 +0400

    Prettier added.

--- test/index.js
+++ test/index.js
@@ -1,34 +1,43 @@
 "use strict";
 
 // Expect module.
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
 // Import source.
-const md5 = require('../src/index');
+const md5 = require("../src/index");
 
 // apache-md5 tests.
-describe('apache-md5', () => {
-    // Test for valid password.
-    it('valid password', () => {
-        const encrypted = md5("su/P3R%se#ret!", "$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
-        expect(encrypted).to.equal("$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
-    });
+describe("apache-md5", () => {
+  // Test for valid password.
+  it("valid password", () => {
+    const encrypted = md5(
+      "su/P3R%se#ret!",
+      "$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/"
+    );
+    expect(encrypted).to.equal("$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
+  });
 
-    // Test for valid short password.
-    it('valid short password', () => {
-        const encrypted = md5("123456", "$1$VV5.4y5.$JbhytGQBPmDHBbrSjF2i7.");
-        expect(encrypted).to.equal("$1$VV5.4y5.$JbhytGQBPmDHBbrSjF2i7.");
-    });
+  // Test for valid short password.
+  it("valid short password", () => {
+    const encrypted = md5("123456", "$1$VV5.4y5.$JbhytGQBPmDHBbrSjF2i7.");
+    expect(encrypted).to.equal("$1$VV5.4y5.$JbhytGQBPmDHBbrSjF2i7.");
+  });
 
-    // Test for invalid password.
-    it('invalid password', () => {
-        const encrypted = md5("invalidPass", "$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
-        expect(encrypted).to.not.equal("$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
-    });
+  // Test for invalid password.
+  it("invalid password", () => {
+    const encrypted = md5(
+      "invalidPass",
+      "$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/"
+    );
+    expect(encrypted).to.not.equal("$apr1$cF.rAvCe$YlzjmK4qu/ia6hC8CNfnm/");
+  });
 
-    // Test for invalid short password.
-    it('invalid short password', () => {
-        const encrypted = md5("passw0rdpa55wore", "$1$VV5.4y5.$9981ZZhKTHmeXFKQur4cV0");
-        expect(encrypted).to.not.equal("$1$VV5.4y5.$9981ZZhKTHmeXFKQur4cV0");
-    });
+  // Test for invalid short password.
+  it("invalid short password", () => {
+    const encrypted = md5(
+      "passw0rdpa55wore",
+      "$1$VV5.4y5.$9981ZZhKTHmeXFKQur4cV0"
+    );
+    expect(encrypted).to.not.equal("$1$VV5.4y5.$9981ZZhKTHmeXFKQur4cV0");
+  });
 });
